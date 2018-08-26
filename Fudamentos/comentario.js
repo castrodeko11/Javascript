81ae03b3f1c83d75e954c8e2a92b13d65ec080af
André Oliveira <castrodeko11@hotmail.com>
2018-08-25 23:15:07 -0300

    Javascript e Node Estrutura e comentários

---
+++ Fudamentos/comentario.js
@@ -0,0 +1,16 @@
+// Comentário de uma Linha
+console.log('Linha 1')
+
+/*
+    Comentário de 
+    Múltiplas linhas
+*/
+
+console.log('Linha 2')
+
+/*
+*Comentário de
+*Múltiplas Linhas
+*/
+
+console.log('Linha 3')
