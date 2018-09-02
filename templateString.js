61329e6ae94e8f3d5c13ab70cdb7d00ce097356e
André Oliveira <castrodeko11@hotmail.com>
2018-09-02 08:29:41 -0300

    Javascript Template string

---
+++ templateString.js
@@ -0,0 +1,14 @@
+const nome = 'Rebeca'
+const concatenacao = 'olá' + nome + '!'
+const templat = `
+    olá
+    ${nome}!`
+console.log(concatenacao, templat)
+
+// expressões.......
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up  = texto => texto.toUpperCase()
+console.log(`Ei........${(up('cuidado'))}`)
+
+
