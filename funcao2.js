c4339e94d14878118e5eb87ba194bbcb5281d050
André Oliveira <castrodeko11@hotmail.com>
2018-09-30 01:18:06 -0300

    Funções 2 - armazenar na variavel e arrow

---
+++ funcao2.js
@@ -0,0 +1,19 @@
+// Armazenado uma funcao em uma variavel
+const imprimirSoma = function (a,b){
+
+    console.log(a+b)
+}
+
+imprimirSoma(2,3)
+
+// Armazenando uma funcao arrow em uma varivael
+const soma = (a,b) => {
+    return a + b
+}
+
+console.log(soma(2,3))
+
+// retorno implícito
+const subtracao = (a,b) => a - b
+console.log(subtracao(2,5));
+
